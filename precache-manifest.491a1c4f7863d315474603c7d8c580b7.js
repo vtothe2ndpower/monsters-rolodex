1cdfc40aec8fa3706bc560ac56e2041ebfd0b4be
Victor Vargas <vvargasnunez@gmail.com>
2020-08-17 11:48:56 -0700

    Updates

---
+++ precache-manifest.491a1c4f7863d315474603c7d8c580b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64572d326a9a41a23e25987ac57db096",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "4ead323de40be56af018",
+    "url": "/monsters-rolodex/static/css/main.02b0b803.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ead323de40be56af018",
+    "url": "/monsters-rolodex/static/js/main.3ba31042.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
